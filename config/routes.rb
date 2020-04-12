3471b93a7d9e0d73189f94c5e2083a9117d8a604
joyfulben <benfishernow@gmail.com>
2020-04-12 16:30:26 -0600

    installed gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'postgres://xublevljvumfzm:1dec1cb0e42feedbdbe6938c37cd205a576f5193a173a89976ce1f8fdea9ab3b@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d8igek20jhj3i'
+  root '#'
   resources :site_outputs
   resources :users do
     collection do
